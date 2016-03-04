ff56806e294a70344ab06040b2fc73ef6bfd5c70
GaHoKwan <kwangaho@gmail.com>
2016-03-04 10:24:21 +0000

    Revert "klte-common: Customize for felicia jpn device"
    This reverts commit eb2cdd238464c2dc8e1b3f763384449eb9d2b919.

--- klte.mk
+++ klte.mk
@@ -43,6 +43,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:system/etc/permissions/android.hardware.consumerir.xml \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
     frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:system/etc/permissions/android.hardware.sensor.stepcounter.xml \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:system/etc/permissions/android.hardware.sensor.stepdetector.xml \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
@@ -97,20 +98,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
 
 # NFC
-ifeq ($(DEVICE_NFC_SONY),yes)
-PRODUCT_PACKAGES += \
-    com.android.nfc_extras \
-    NfcSony \
-    Tag
-else
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml
-
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
     NfcNci \
     Tag
-endif
 
 # Radio
 PRODUCT_PACKAGES += \
